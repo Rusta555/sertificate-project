7f98a66a45841c97f09e59cd160dea590d1652e1
Rustam Cafarov <RustamCafarov@embawood.dm>
2023-08-21 16:47:29 +0400

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,41 @@
+function Header () {
+    return (
+        
+            <ul className="header">
+                <li>
+                    Mac
+                </li>
+                <li>
+                    iPad
+                </li>
+                <li>
+                    iPhone
+                </li>
+                <li>
+                    Watch
+                </li>
+                <li>
+                    TV
+                </li>
+                <li>
+                    Airpods
+                </li>
+                <li>
+                    Accessories
+                </li>
+                <li>
+                    Brands
+                </li>
+                <li>
+                    Services
+                </li>
+                <li>
+                    Service
+                </li>
+            
+            </ul>
+       
+    );
+}
+
+export default Header;
